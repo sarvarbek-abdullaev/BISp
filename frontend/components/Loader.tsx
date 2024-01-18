37047712a730fe076b51ad7b18639db17015c4b2
sarvarbek-abdullaev <sarvar.bek.790@gmail.com>
2024-01-18 05:29:40 +0500

    fix: Loader centered

--- frontend/components/Loader.tsx
+++ frontend/components/Loader.tsx
@@ -1,10 +1,10 @@
 import { Spinner } from '@chakra-ui/spinner';
-import { Center } from '@chakra-ui/react';
+import { Center, Flex } from '@chakra-ui/react';
 
 export default function LoadingSkeleton() {
   return (
-    <Center height="100vh" width="100%">
+    <Flex width="100%" justifyContent="center" alignItems="center">
       <Spinner thickness="4px" speed="0.6s" emptyColor="gray.200" color="blue.500" size="xl" />
-    </Center>
+    </Flex>
   );
 }
