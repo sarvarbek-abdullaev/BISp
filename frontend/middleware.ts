6feac790102c83a0231d1168474b33d44af40c1e
sarvarbek-abdullaev <sarvar.bek.790@gmail.com>
2024-01-18 05:11:43 +0500

    feat: middleware logic to detect and redirect admin or users page according to session role

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,15 +1,38 @@
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+import { usage } from 'browserslist';
 
-export function middleware(request: NextRequest) {
-  if (!request.cookies.has('next-auth.session-token') && !request.nextUrl.pathname.includes('api')) {
-    return NextResponse.redirect(new URL('/auth/login', request.url));
-  } else if (request.nextUrl.pathname === '/') {
-    return NextResponse.redirect(new URL('/dashboard', request.url));
+const secret = process.env.NEXTAUTH_SECRET;
+
+export async function middleware(req: NextRequest) {
+  const currentPath = req.nextUrl.pathname;
+  const session = await getToken({ req, secret });
+
+  if (currentPath.startsWith('/api')) {
+    return NextResponse.next();
+  }
+
+  if (!req.cookies.has('next-auth.session-token') && !currentPath.includes('api')) {
+    return NextResponse.redirect(new URL('/auth/login', req.url));
+  }
+
+  if (session) {
+    const {
+      user: { role },
+    } = session;
+
+    const isAdmin = role.toLowerCase() === 'admin';
+
+    if (isAdmin && !currentPath.startsWith('/admin')) {
+      return NextResponse.redirect(new URL('/admin', req.url));
+    }
+    if (!isAdmin && (currentPath === '/' || currentPath.startsWith('/admin'))) {
+      return NextResponse.redirect(new URL('/dashboard', req.url));
+    }
   }
 }
 
 export const config = {
-  // matcher: ['/admin/:path*', '/dashboard', '/profile', '/timetable', '/events', '/settings'],
-  matcher: ['/((?!.+\\.[\\w]+$|_next|auth/login|api).*)', '/(api|trpc)(.*)'],
+  matcher: ['/((?!.+\\.[\\w]+$|_next|auth/login).*)', '/(admin|trpc)(.*)'],
 };
