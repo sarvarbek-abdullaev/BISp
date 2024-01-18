cc965f383dfbe6b6b433c32abb07c7965c4acaca
sarvarbek-abdullaev <sarvar.bek.790@gmail.com>
2024-01-18 05:31:45 +0500

    feat: flex updated to wrapper

--- frontend/app/admin/groups/[id]/layout.tsx
+++ frontend/app/admin/groups/[id]/layout.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import { Flex } from '@chakra-ui/react';
 import GroupSidebar from '@/app/admin/groups/group-sidebar';
+import { Wrapper } from '@/components/Wrapper';
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -10,9 +11,7 @@ const AdminLayout: FC<LayoutProps> = async ({ children }) => {
   return (
     <>
       <GroupSidebar />
-      <Flex direction="column" p="5" w="90%" marginX="10" bg="#202020" borderRadius="8px" overflow="hidden">
-        {children}
-      </Flex>
+      <Wrapper>{children}</Wrapper>
     </>
   );
 };
