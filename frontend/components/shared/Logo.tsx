b22a9eea6278486ff357ccaa10ddb60f286be73f
sarvarbek-abdullaev <sarvar.bek.790@gmail.com>
2024-02-17 19:07:09 +0500

    feat: new logo

--- frontend/components/shared/Logo.tsx
+++ frontend/components/shared/Logo.tsx
@@ -1,1 +1,14 @@
-export const Logo = () => <div>Edu Connect</div>;
+import Image from 'next/image';
+import { FC } from 'react';
+
+const LOGO_URL = 'https://utfs.io/f/fbeb848a-b67a-4614-aed9-ab5871529b35-x581ka.png';
+
+interface LogoProps {
+  className?: string;
+  width?: number;
+  height?: number;
+}
+
+export const Logo: FC<LogoProps> = ({ className, height, width }) => (
+  <Image src={LOGO_URL} className={className} alt="Logo" width={width} height={height} />
+);
