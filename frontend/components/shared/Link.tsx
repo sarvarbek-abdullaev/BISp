0669593f5fed1389470906c0963af70b6f0e5ebf
sarvarbek-abdullaev <sarvar.bek.790@gmail.com>
2024-02-19 20:06:36 +0500

    feat: Indicator for Link

--- frontend/components/shared/Link.tsx
+++ frontend/components/shared/Link.tsx
@@ -1,17 +1,28 @@
 'use client';
 import NextLink from 'next/link';
-import { FC } from 'react';
+import React, { FC } from 'react';
 import { usePathname } from 'next/navigation';
+import { cn } from '@/lib/utils';
 
 interface LinkProps {
   href: string;
   children: React.ReactNode;
   query?: string;
+  indicator?: boolean;
+  className?: string;
 }
-const Link: FC<LinkProps> = ({ href, children, query }) => {
+const Link: FC<LinkProps> = ({ href, children, className, indicator = false, query }) => {
   const pathname = usePathname();
+
+  const isActive = href.includes(pathname + query) && pathname.includes(href);
   return (
-    <NextLink href={href} className={href.includes(pathname + query) && pathname.includes(href) ? 'disabled' : ''}>
+    <NextLink href={href} className={cn(className, isActive ? 'disabled' : '')}>
+      <div
+        className={cn(
+          'absolute top-0 right-0 w-[5px] rounded-lg h-full hidden sm:block',
+          isActive && indicator && 'bg-black',
+        )}
+      />
       {children}
     </NextLink>
   );
