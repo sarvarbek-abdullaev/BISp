6053df0f8b3c6715cf7d844c4bd0ee7789cd90ec
sarvarbek-abdullaev <sarvar.bek.790@gmail.com>
2024-02-19 19:31:25 +0500

    fix: hydration error

--- frontend/components/shared/Popover.tsx
+++ frontend/components/shared/Popover.tsx
@@ -1,7 +1,6 @@
 import React, { FC } from 'react';
 
 import { Popover as PopoverShadcn, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
-import { Button } from '@/components/ui/button';
 
 interface PopoverProps {
   element: React.ReactNode;
@@ -12,9 +11,7 @@ interface PopoverProps {
 export const Popover: FC<PopoverProps> = ({ element, header, body }) => {
   return (
     <PopoverShadcn>
-      <PopoverTrigger>
-        <Button className="bg-transparent hover:bg-transparent text-white">{element}</Button>
-      </PopoverTrigger>
+      <PopoverTrigger>{element}</PopoverTrigger>
       <PopoverContent className="w-fit">
         {header && <div className="mb-4">{header}</div>}
         {body}
