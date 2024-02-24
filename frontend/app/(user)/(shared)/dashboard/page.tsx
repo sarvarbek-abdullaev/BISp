de3b6dbb9c8a218a03598b79b482f401175ebcfb
sarvarbek-abdullaev <sarvar.bek.790@gmail.com>
2024-02-24 18:23:04 +0500

    feat: Dashboard & style problem

--- frontend/app/(user)/(shared)/dashboard/page.tsx
+++ frontend/app/(user)/(shared)/dashboard/page.tsx
@@ -14,51 +14,51 @@ const Dashboard = async () => {
 
   const currentYear = studentCourseModules[0]?.modules;
 
-  if (!currentYear?.length) {
-    return <div>No Modules Found</div>;
-  }
-
   return (
     <div className="p-10">
       <h1 className="text-4xl font-mono mb-10">My Modules:</h1>
-      <div className="grid grid-cols-3 gap-4">
-        {currentYear?.map((module) => {
-          const moduleLinks = [
-            {
-              href: `/module/${module.id}/marks`,
-              icon: ClipboardCheckIcon,
-              text: 'Marks',
-            },
-            {
-              href: `/module/${module.id}/attendance`,
-              icon: UsersIcon,
-              text: 'Attendance',
-            },
-            {
-              href: `/module/${module.id}/assignments`,
-              icon: FileTextIcon,
-              text: 'Assignments',
-            },
-          ];
+      {!currentYear?.length ? (
+        <div>No Modules Found</div>
+      ) : (
+        <div className="grid grid-cols-3 gap-4">
+          {currentYear?.map((module) => {
+            const moduleLinks = [
+              {
+                href: `/module/${module.id}/marks`,
+                icon: ClipboardCheckIcon,
+                text: 'Marks',
+              },
+              {
+                href: `/module/${module.id}/attendance`,
+                icon: UsersIcon,
+                text: 'Attendance',
+              },
+              {
+                href: `/module/${module.id}/assignments`,
+                icon: FileTextIcon,
+                text: 'Assignments',
+              },
+            ];
 
-          return (
-            <div key={module.id} className="border-2 py-4 px-6 rounded-lg bg-gray-950 text-xl space-y-3">
-              <div className="flex flex-col gap-2">
-                <h2>Name: {module.name}</h2>
-                <p>Code: {module.code}</p>
-                <div className="flex justify-between text-sm mt-10 min-h-16 items-end">
-                  {moduleLinks?.map((link, index) => (
-                    <Link key={index} href={link.href} className="flex flex-col gap-2 items-center text-gray-400">
-                      <link.icon className="mr-1 h-5 w-5" />
-                      <span>{link.text}</span>
-                    </Link>
-                  ))}
+            return (
+              <div key={module.id} className="border-2 py-4 px-6 rounded-lg bg-gray-950 text-xl space-y-3">
+                <div className="flex flex-col gap-2 h-full">
+                  <h2>Name: {module.name}</h2>
+                  <p>Code: {module.code}</p>
+                  <div className="flex justify-between text-sm mt-10 min-h-16 items-end flex-1">
+                    {moduleLinks?.map((link, index) => (
+                      <Link key={index} href={link.href} className="flex flex-col gap-2 items-center text-gray-400">
+                        <link.icon className="mr-1 h-5 w-5" />
+                        <span>{link.text}</span>
+                      </Link>
+                    ))}
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
-      </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
